73b52fc3eb1433da2f8d1e255c66726192bb57f1
jaybaybremner <j.bremner@ymail.com>
2017-02-21 10:05:46 +0000

    dependencies

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,6 @@
-
-
-app.use('/api', expressJWT({ secret: secret })
-  .unless({
-    path: [
-      { url: '/api/login', methods: ['POST'] },
-      { url: '/api/register', methods: ['POST'] }
-    ]
-  }));
+angular
+.module('wdi-project-4', [
+  'ui-router',
+  'ngResource',
+  'angular-jwt'
+]);
